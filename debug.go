b0ce3102edeefbd174b9aab5ef71f0649d7244d0
Thomas Fossati <thomas.fossati@arm.com>
2020-11-10 15:51:29 +0000

    Add a Validate method to the Debug type (#6)

--- debug.go
+++ debug.go
@@ -3,7 +3,9 @@
 
 package eat
 
-import "fmt"
+import (
+	"fmt"
+)
 
 /*
 debug-disable-type = &(
@@ -14,30 +16,49 @@ debug-disable-type = &(
     full-permanent-disable: 4
 )
 */
+
 const (
+	// DebugNotDisabled is asserted if any debug facility, even manufacturer
+	// hardware diagnostics, is currently enabled
 	DebugNotDisabled = iota
+
+	// DebugDisabled indicates all debug facilities are currently disabled. It
+	// may be possible to enable them in the future, and it may also be possible
+	// that they were enabled in the past after the target device/sub-system
+	// booted/started, but they are currently disabled.
 	DebugDisabled
+
+	// DebugDisabledSinceBoot indicates all debug facilities are currently
+	// disabled and have been so since the target device/sub-system
+	// booted/started.
 	DebugDisabledSinceBoot
+
+	// DebugPermanentDisable indicates all non-manufacturer facilities are
+	// permanently disabled such that no end user or developer cannot enable
+	// them. Only the manufacturer indicated in the OEMID claim can enable them.
+	// This also indicates that all debug facilities are currently disabled and
+	// have been so since boot/start.
 	DebugPermanentDisable
+
+	// DebugFullPermanentDisable indicates that all debug capabilities for the
+	// target device/sub-module are permanently disabled.
 	DebugFullPermanentDisable
 )
 
 // Debug models the debug-disable type
 type Debug uint
 
-// Set makes sure that the supplied val makes a good Debug claim
-func (d *Debug) Set(val uint) error {
-	switch val {
+// Validate makes sure that the receiver is a valid Debug claim
+func (d Debug) Validate() error {
+	switch d {
 	case DebugNotDisabled:
 	case DebugDisabled:
 	case DebugDisabledSinceBoot:
 	case DebugPermanentDisable:
 	case DebugFullPermanentDisable:
 	default:
-		return fmt.Errorf("out of range value %v for Debug type", val)
+		return fmt.Errorf("out of range value %v for Debug type", d)
 	}
-
-	*d = Debug(val)
 
 	return nil
 }
