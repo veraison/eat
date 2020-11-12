dd6ee25a3a0525e269430fb964de23c32f492939
setrofim <setrofim@gmail.com>
2020-11-12 11:20:01 +0000

    Add NumericDate unmarshallers
    Add UnmarshalJSON and UnmarshalCBOR to NumericDate type.

--- numericdate.go
+++ numericdate.go
@@ -20,6 +20,21 @@ func (nd NumericDate) MarshalJSON() ([]byte, error) {
 	return []byte(strconv.FormatInt(t.Unix(), 10)), nil
 }
 
+// UnmarshalJSON populates the receiver NumericDate by interpreting the
+// supplied data as UTF-8-encoded Unix timestamp.
+func (nd *NumericDate) UnmarshalJSON(data []byte) error {
+	s := string(data)
+
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return err
+	}
+
+	*nd = NumericDate(time.Unix(int64(i), 0))
+
+	return nil
+}
+
 // MarshalCBOR unwraps the receiver NumericDate exposing its underlying Time
 // and dispatches it to our custom encoding mode, which automatically adds the
 // required tag (TimeTag == cbor.EncTagRequired)
@@ -27,4 +42,17 @@ func (nd NumericDate) MarshalCBOR() ([]byte, error) {
 	t := time.Time(nd)
 
 	return em.Marshal(t)
+}
+
+// UnmarshalCBOR decodes the data into a Time and sets the receiver NumericDate
+// to the decoded value.
+func (nd *NumericDate) UnmarshalCBOR(data []byte) error {
+	var t time.Time
+	if err := dm.Unmarshal(data, &t); err != nil {
+		return err
+	}
+
+	*nd = NumericDate(t)
+
+	return nil
 }
